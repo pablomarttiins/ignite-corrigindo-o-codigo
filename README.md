ae3307531c41abec1ca772929ae674a33fd4e81f
Pablo Martins <pablo.martins@hotmail.com.br>
2021-05-14 00:31:52 -0300

    Finalização teste

---
+++ README.md
@@ -0,0 +1,18 @@
+<h2 align="center">
+  Corrigindo o código
+</h2>
+
+
+## Executando o projeto
+Instale as dependências do projeto
+```
+yarn install
+```
+Inicie a aplicação utilizando o comando yarn
+```
+yarn dev
+```
+Para executar os testes do projeto, basta rodar o comando
+```
+yarn test
+```
--- src/index.js
+++ src/index.js
@@ -23,14 +23,20 @@ app.post("/repositories", (request, response) => {
     likes: 0
   };
 
-  return response.json(repository);
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
   const { id } = request.params;
   const updatedRepository = request.body;
 
-  repositoryIndex = repositories.findindex(repository => repository.id === id);
+  if (updatedRepository?.likes > 0) {
+    delete updatedRepository.likes
+  }
+
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
   if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
@@ -46,9 +52,9 @@ app.put("/repositories/:id", (request, response) => {
 app.delete("/repositories/:id", (request, response) => {
   const { id } = request.params;
 
-  repositoryIndex = repositories.findIndex(repository => repository.id === id);
+  const repositoryIndex = repositories.findIndex(repository => repository.id === id);
 
-  if (repositoryIndex > 0) {
+  if (repositoryIndex < 0) {
     return response.status(404).json({ error: "Repository not found" });
   }
 
@@ -66,9 +72,10 @@ app.post("/repositories/:id/like", (request, response) => {
     return response.status(404).json({ error: "Repository not found" });
   }
 
-  const likes = ++repositories[repositoryIndex].likes;
+  repositories[repositoryIndex].likes++;
+  const likes = repositories[repositoryIndex].likes;
 
-  return response.json('likes');
+  return response.json({likes: likes});
 });
 
 module.exports = app;
